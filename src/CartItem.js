7698a82b53d79e3a4620a4956bb0005d1b2bb261
VishalRohilla07 <Rohillavishal12@gmail.com>
2021-09-10 18:34:48 +0530

    add increased quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,23 +1,43 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 9999,
+            title: 'Phone',
+            qty: 1,
+            img:''
+        }
+        //we can also use this method to bind
+        //this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity= () =>{
+        console.log('this',this.state);
+    }
+
+
     render(){
+        const {price,title,qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25 }}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 9999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25 }}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className ="cart-item-actions">
                         {/*Buttons*/}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQuantity}/>
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"onClick={this.decreaseQuantity}/>
+                        <img alt="delete" className="action-icons" src="https://img-premium.flaticon.com/png/512/484/premium/484662.png?token=exp=1631268370~hmac=5157f143bc678b6cadfa0e12540c748e"/>
                     </div>
                 </div>
             </div>
         );
-    }
+    } 
 }
 
 const styles = {
